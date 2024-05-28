7798cfd6d1a34f3bdc343599e0c6eae06c12b3d8
Diego Jerez <diegoje@microsoft.com>
2024-05-28 16:26:38 +0100

    cleanup

--- spm-integration-test.sh
+++ spm-integration-test.sh
@@ -1,59 +1,45 @@
-# We are in source directory
-
-#BRANCH_NAME="ci/testspm"
-#BRANCH_NAME="$(Build.SourceBranch)-ci-testspm"
 UUID=$(uuidgen)
-BRANCH_NAME="${UUID}-ci-testspm"
+BRANCH_NAME="ci-testspm-temp-${UUID}"
 SAMPLE_APP_TEMP_DIR="NativeAuthSampleAppTemp"
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
 
-#######
-## 1 ##
-#######
+# Build framework
 
 git checkout -b "$BRANCH_NAME"
 rm -rf archive framework MSAL.zip
 
-echo "DJB: Build for simulator"
-
 xcodebuild -sdk iphonesimulator -configuration Release -workspace MSAL.xcworkspace -scheme "MSAL (iOS Framework)" archive SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES -archivePath archive/iOSSimulator CODE_SIGNING_ALLOWED=NO -quiet > build.log 2>&1
 XCBUILD_STATUS_SIM=$?
 if [ $XCBUILD_STATUS_SIM -ne 0 ]; then
-  echo "** BUILD FAILED **"
+  echo "** Build xcframework Error **"
   exit 1
 fi
-
-echo "DJB: Build for iOS"
 
 xcodebuild -sdk iphoneos -configuration Release -workspace MSAL.xcworkspace -scheme "MSAL (iOS Framework)" archive SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES -archivePath archive/iOS CODE_SIGNING_ALLOWED=NO -quiet > build.log 2>&1
 XCBUILD_STATUS_IPHONE=$?
 if [ $XCBUILD_STATUS_IPHONE -ne 0 ]; then
-  echo "** BUILD FAILED **"
+  echo "** Build xcframework Error **"
   exit 1
 fi
-
-echo "DJB: Build for Mac"
 
 xcodebuild -sdk macosx -configuration Release -workspace MSAL.xcworkspace -scheme "MSAL (Mac Framework)" archive SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES -archivePath archive/macOS CODE_SIGNING_ALLOWED=NO -quiet > build.log 2>&1
 XCBUILD_STATUS_MAC=$?
 if [ $XCBUILD_STATUS_MAC -ne 0 ]; then
-  echo "** BUILD FAILED **"
+  echo "** Build xcframework Error **"
   exit 1
 fi
-
-echo "DJB: Start building the framework"
 
 xcodebuild -create-xcframework -framework archive/iOSSimulator.xcarchive/Products/Library/Frameworks/MSAL.framework -framework archive/iOS.xcarchive/Products/Library/Frameworks/MSAL.framework -framework archive/macOS.xcarchive/Products/Library/Frameworks/MSAL.framework -output framework/MSAL.xcframework > build.log 2>&1
 XCBUILD_STATUS_FRAMEWORK=$?
 if [ $XCBUILD_STATUS_FRAMEWORK -ne 0 ]; then
-  echo "** BUILD FAILED **"
+  echo "** Build xcframework Error **"
   exit 1
 fi
 
 zip -r MSAL.zip framework/MSAL.xcframework -y -v
 CHECKSUM=$(swift package compute-checksum MSAL.zip)
 if [ -z "$CHECKSUM" ]; then
-  echo "** ERROR CHECKSUM **"
+  echo "** Checksum could not be obtained **"
   exit 1
 fi
 
@@ -69,9 +55,7 @@ git push -f origin "$BRANCH_NAME"
 
 echo "MSAL.zip pushed to temporary branch: $BRANCH_NAME"
 
-#######
-## 2 ##
-#######
+# Download Sample App
 
 mkdir -p "$SAMPLE_APP_TEMP_DIR"
 cd "$SAMPLE_APP_TEMP_DIR"
@@ -82,30 +66,25 @@ cd ms-identity-ciam-native-auth-ios-sample
 sed -i '' 's#kind = upToNextMinorVersion;#kind = branch;#' NativeAuthSampleApp.xcodeproj/project.pbxproj
 sed -i '' "s#minimumVersion = [0-9.]*;#branch = $BRANCH_NAME;#" NativeAuthSampleApp.xcodeproj/project.pbxproj
 
-echo "Reset Sample App's Package cache"
-
 rm -f NativeAuthSampleApp.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
 
-# DJB: If it doesn't work, remove the derived data from Xcode here
 xcodebuild -resolvePackageDependencies
 xcodebuild -scheme NativeAuthSampleApp -configuration Release -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=latest' clean build
 BUILD_STATUS=$?
 
-#######
-## 3 ##
-#######
+# Cleanup
 
 cd ../..
 rm -rf "SAMPLE_APP_TEMP_DIR" archive framework MSAL.zip
 
 git checkout -- .
-git fetch
+#git fetch
 git switch main
 
 git branch -D "$BRANCH_NAME"
 git push origin --delete "$BRANCH_NAME"
 
 if $BUILD_STATUS -ne 0 ]; then
-	echo "** Sample App Build error **"
+	echo "** Sample App build error **"
 	exit 1
 fi
