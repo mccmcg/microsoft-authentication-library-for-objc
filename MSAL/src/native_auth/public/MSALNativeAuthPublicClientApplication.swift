cd4012e9e7820c4d363353426acabfeb8cf692e8
Diego Jerez <diegoje@microsoft.com>
2024-05-28 16:12:33 +0100

    wip

--- MSAL/src/native_auth/public/MSALNativeAuthPublicClientApplication.swift
+++ MSAL/src/native_auth/public/MSALNativeAuthPublicClientApplication.swift
@@ -89,8 +89,6 @@ public final class MSALNativeAuthPublicClientApplication: MSALPublicClientApplic
         try super.init(configuration: config)
     }
 
-    will not build
-
     /// Initialize a MSALNativePublicClientApplication.
     /// - Parameters:
     ///   - clientId: The client ID of the application, this should come from the app developer portal.
